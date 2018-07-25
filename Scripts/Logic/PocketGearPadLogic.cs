a852d837f49878f0332311d5396dcdc106f5f155
Sisk S'jet <sisk.sjet@gmail.com>
2018-07-25 06:02:17 +0200

    feat: add an auto place functionality for PocketGear Pads

---
+++ Scripts/Logic/PocketGearPadLogic.cs
@@ -0,0 +1,54 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sisk.Utils.Logging;
+using Sisk.Utils.Profiler;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using IMyLandingGear = SpaceEngineers.Game.ModAPI.IMyLandingGear;
+
+namespace AutoMcD.PocketGear.Logic {
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_LandingGear), false, POCKETGEAR_PAD, POCKETGEAR_PAD_LARGE, POCKETGEAR_PAD_LARGE_SMALL, POCKETGEAR_PAD_SMALL)]
+    public class PocketGearPadLogic : MyGameLogicComponent {
+        public const string POCKETGEAR_PAD = "MA_PocketGear_Pad";
+        public const string POCKETGEAR_PAD_LARGE = "MA_PocketGear_L_Pad";
+        public const string POCKETGEAR_PAD_LARGE_SMALL = "MA_PocketGear_L_Pad_sm";
+        public const string POCKETGEAR_PAD_SMALL = "MA_PocketGear_Pad_sm";
+        private IMyLandingGear _pocketGearPad;
+        protected ILogger Log { get; set; }
+
+        public override void Close() {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(PocketGearPadLogic), nameof(Close)) : null) {
+                // bug: throws: Cannot bind to the target method because its signature or security transparency is not compatible with that of the delegate type.
+                //_pocketGearPad.LockModeChanged -= OnLockModeChanged;
+            }
+        }
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder) {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(PocketGearPadLogic), nameof(Init)) : null) {
+                Log = Mod.Static.Log.ForScope<PocketGearPadLogic>();
+                Log.Debug(Entity.GetType().ToString());
+                _pocketGearPad = Entity as IMyLandingGear;
+
+                NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+            }
+        }
+
+        public override void UpdateOnceBeforeFrame() {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(PocketGearPadLogic), nameof(UpdateOnceBeforeFrame)) : null) {
+                // bug: throws: Cannot bind to the target method because its signature or security transparency is not compatible with that of the delegate type.
+                //_pocketGearPad.LockModeChanged += OnLockModeChanged;
+            }
+        }
+
+        private void OnLockModeChanged(IMyLandingGear gear, LandingGearMode mode) {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(PocketGearPadLogic), nameof(Close)) : null) {
+                Log.Debug($"Landing gear mode: {mode}");
+                if (mode == LandingGearMode.Locked) {
+                    // note: maybe we can use it to stop or reduce clang if the bug gets fixed.
+                    //_pocketGearPad.CubeGrid.Physics.ClearSpeed();
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Logic/PocketGearPartLogic.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using Sisk.Utils.Logging;
+using Sisk.Utils.Profiler;
+using VRage;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRageMath;
+
+namespace AutoMcD.PocketGear.Logic {
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_MotorAdvancedRotor), false, POCKETGEAR_PART, POCKETGEAR_PART_LARGE, POCKETGEAR_PART_SMALL, POCKETGEAR_PART_LARGE_SMALL)]
+    public class PocketGearPartLogic : MyGameLogicComponent {
+        public const string POCKETGEAR_PART = "MA_PocketGear_Rotor";
+        public const string POCKETGEAR_PART_LARGE = "MA_PocketGear_L_Rotor";
+        public const string POCKETGEAR_PART_LARGE_SMALL = "MA_PocketGear_L_Rotor_sm";
+        public const string POCKETGEAR_PART_SMALL = "MA_PocketGear_Rotor_sm";
+        private IMyMotorRotor _pocketGearPart;
+
+        protected ILogger Log { get; set; }
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder) {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(PocketGearPartLogic), nameof(Init)) : null) {
+                Log = Mod.Static.Log.ForScope<PocketGearPartLogic>();
+
+                _pocketGearPart = Entity as IMyMotorRotor;
+                NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+            }
+        }
+
+        public override void UpdateOnceBeforeFrame() {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(PocketGearPartLogic), nameof(UpdateOnceBeforeFrame)) : null) {
+                if (_pocketGearPart?.CubeGrid?.Physics == null) {
+                    return;
+                }
+
+                try {
+                    PlaceLandingPad();
+                } catch (Exception exception) {
+                    Log.Error(exception);
+                }
+            }
+        }
+
+        private void PlaceLandingPad() {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(PocketGearPartLogic), nameof(PlaceLandingPad)) : null) {
+                var cubeGrid = _pocketGearPart.CubeGrid;
+                var gridSize = cubeGrid.GridSize;
+                var left = _pocketGearPart.WorldMatrix.Left;
+                var forward = _pocketGearPart.WorldMatrix.Forward;
+                var up = _pocketGearPart.WorldMatrix.Up;
+
+                var position = _pocketGearPart.GetPosition();
+
+                Vector3D origin;
+                string pocketGearPadId;
+                switch (_pocketGearPart.BlockDefinition.SubtypeId) {
+                    case POCKETGEAR_PART:
+                        pocketGearPadId = PocketGearPadLogic.POCKETGEAR_PAD;
+                        origin = position + left * gridSize * 2 + forward * gridSize;
+                        break;
+                    case POCKETGEAR_PART_LARGE:
+                        pocketGearPadId = PocketGearPadLogic.POCKETGEAR_PAD_LARGE;
+                        origin = position + left * (gridSize * 5) + forward * gridSize;
+                        break;
+                    case POCKETGEAR_PART_SMALL:
+                        pocketGearPadId = PocketGearPadLogic.POCKETGEAR_PAD_SMALL;
+                        origin = position + left + forward * gridSize;
+                        break;
+                    case POCKETGEAR_PART_LARGE_SMALL:
+                        pocketGearPadId = PocketGearPadLogic.POCKETGEAR_PAD_LARGE_SMALL;
+                        origin = position + left * (gridSize * 5) + forward * gridSize;
+                        break;
+                    default:
+                        throw new Exception($"Unknown PocketGearPart SubtypeId: {_pocketGearPart.BlockDefinition.SubtypeId}");
+                }
+
+                var padPosition = cubeGrid.WorldToGridInteger(origin);
+                var canPlaceCube = cubeGrid.CanAddCube(padPosition);
+                if (canPlaceCube) {
+                    try {
+                        var landingGearBuilder = new MyObjectBuilder_LandingGear {
+                            SubtypeName = pocketGearPadId,
+                            Owner = _pocketGearPart.OwnerId,
+                            BuiltBy = _pocketGearPart.OwnerId,
+                            AutoLock = true
+                        };
+
+                        var cubeGridBuilder = new MyObjectBuilder_CubeGrid {
+                            CreatePhysics = true,
+                            GridSizeEnum = _pocketGearPart.CubeGrid.GridSizeEnum,
+                            PositionAndOrientation = new MyPositionAndOrientation(origin, forward, up)
+                        };
+
+                        cubeGridBuilder.CubeBlocks.Add(landingGearBuilder);
+
+                        var gridsToMerge = new List<MyObjectBuilder_CubeGrid> { cubeGridBuilder };
+                        (cubeGrid as MyCubeGrid)?.PasteBlocksToGrid(gridsToMerge, 0, false, false);
+                    } catch (Exception exception) {
+                        Log.Error(exception);
+                    }
+                }
+            }
+        }
+    }
+}
