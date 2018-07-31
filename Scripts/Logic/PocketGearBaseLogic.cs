113e873c419d4e91ad654fee36f88bd29ab2fafc
Sisk S'jet <sisk.sjet@gmail.com>
2018-07-31 17:55:11 +0200

    chore: changed properties to set initial limits because of an bug

--- Scripts/Logic/PocketGearBaseLogic.cs
+++ Scripts/Logic/PocketGearBaseLogic.cs
@@ -28,7 +28,10 @@ namespace AutoMcD.PocketGear.Logic {
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_MotorAdvancedStator), false, POCKETGEAR_BASE, POCKETGEAR_BASE_LARGE, POCKETGEAR_BASE_LARGE_SMALL, POCKETGEAR_BASE_SMALL)]
     public class PocketGearBaseLogic : MyGameLogicComponent {
         private const float FORCED_LOWER_LIMIT_DEG = 333.5f;
+        private const float FORCED_LOWER_LIMIT_RAD = (float) (Math.PI / 180) * FORCED_UPPER_LIMIT_DEG;
         private const float FORCED_UPPER_LIMIT_DEG = 360.0f;
+        private const float FORCED_UPPER_LIMIT_RAD = (float) (Math.PI / 180) * FORCED_UPPER_LIMIT_DEG;
+
         private const string POCKETGEAR_BASE = "MA_PocketGear_Base";
         private const string POCKETGEAR_BASE_LARGE = "MA_PocketGear_L_Base";
         private const string POCKETGEAR_BASE_LARGE_SMALL = "MA_PocketGear_L_Base_sm";
@@ -393,8 +396,9 @@ namespace AutoMcD.PocketGear.Logic {
                     SwitchDeployState(true);
                 }
 
-                _pocketGearBase.LowerLimitDeg = FORCED_LOWER_LIMIT_DEG;
-                _pocketGearBase.UpperLimitDeg = FORCED_UPPER_LIMIT_DEG;
+                // bug: ImyRotorStator.UpperLimitDeg requirs an radian.
+                _pocketGearBase.LowerLimitRad = FORCED_LOWER_LIMIT_RAD;
+                _pocketGearBase.UpperLimitRad = FORCED_UPPER_LIMIT_RAD;
 
                 if (_pocketGearBase.TopGrid != null) {
                     _lastKnownTopGridId = _pocketGearBase.TopGrid.EntityId;
