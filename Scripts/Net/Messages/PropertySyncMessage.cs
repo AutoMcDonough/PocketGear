77d74836f446ec88c7229a0e697c1593211813ab
Sisk S'jet <sisk.sjet@gmail.com>
2018-10-24 11:38:47 +0200

    chore: remove old network stack and make use of mod utils

---
+++ Scripts/Net/Messages/PropertySyncMessage.cs
@@ -0,0 +1,23 @@
+﻿using ProtoBuf;
+using Sandbox.ModAPI;
+using Sisk.Utils.Net.Messages;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace AutoMcD.PocketGear.Net.Messages {
+    [ProtoContract]
+    public class PropertySyncMessage : IEntityMessage {
+        [ProtoMember(2)]
+        public string Name { get; set; }
+
+        [ProtoMember(3)]
+        public byte[] Value { get; set; }
+
+        [ProtoMember(1)]
+        public long EntityId { get; set; }
+
+        public byte[] Serialize() {
+            return MyAPIGateway.Utilities.SerializeToBinary(this);
+        }
+    }
+}
